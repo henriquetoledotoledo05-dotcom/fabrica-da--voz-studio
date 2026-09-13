8d2d251543d1ab20b3406b6b21015bd262f8efe8
Henrique Toledo <henriquetoledotoledo05@gmail.com>
2026-09-13 18:07:12 -0300

    Corrigir host do Render

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
-import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+
+  preview: {
+    host: '0.0.0.0',
+    allowedHosts: ['fabrica-da-voz-studio.onrender.com'],
+  },
+
+  server: {
+    host: '0.0.0.0',
+    allowedHosts: ['fabrica-da-voz-studio.onrender.com'],
+  },
 })
