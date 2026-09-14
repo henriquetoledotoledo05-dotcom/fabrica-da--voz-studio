48b8e19999c7391f673648b30a896382718f51e9
Henrique Toledo <henriquetoledotoledo05@gmail.com>
2026-09-14 00:21:46 -0300

    fix dominio personalizado

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,19 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
+  server: {
+    host: '0.0.0.0',
+    allowedHosts: [
+      'fabricadavozstudio.com.br',
+      'www.fabricadavozstudio.com.br'
+    ]
+  },
 
   preview: {
     host: '0.0.0.0',
-    allowedHosts: ['fabrica-da-voz-studio.onrender.com'],
-  },
-
-  server: {
-    host: '0.0.0.0',
-    allowedHosts: ['fabrica-da-voz-studio.onrender.com'],
-  },
+    allowedHosts: [
+      'fabricadavozstudio.com.br',
+      'www.fabricadavozstudio.com.br'
+    ]
+  }
 })
